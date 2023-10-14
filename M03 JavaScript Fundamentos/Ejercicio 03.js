eb709b24c223e4eb6a4d319ce69b9240d92660fa
JosueMorales27 <david2003jdmm@gmail.com>
2023-10-14 02:20:38 -0700

    el ultimo pipazo,acabamos toda la tarea alaverga y toda la puta procastinacionm, sopmos el rey alñaverga, viva dios baruj hasehm, estoy muy feliz, ahora , hare ejercicio bn cabron y a dormir, toda raba aba kadosh .

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -27,7 +27,7 @@ function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
-   return num > 90;
+   return num > 50;
 }
 
 function esPar(num) {
